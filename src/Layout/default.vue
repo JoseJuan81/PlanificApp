96afffd15939aed00db98902135614573dc0b4ac
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-08-13 20:58:38 -0500

    instalacion de componente

--- src/Layout/default.vue
+++ src/Layout/default.vue
@@ -1,15 +1,130 @@
 <template>
   <div>
-    Este layout se manejara router-views con nombres.
-    De esta manera se tendrán diferentes menus y contenidos sin problemas
-    <router-view />
+    <nav>Barra de navegación</nav>
+    <div class="container">
+      <UserProfileLayout
+        class="profile"
+        :show="show"
+        :break-point="768"
+      >
+        <template v-slot:asideLeft>
+          <div class="max-w-sm ml-auto">
+            Izquierda
+            <router-view name="menu-left" />
+          </div>
+        </template>
+        <template v-slot:main-content>
+          <div class="min-w-movil">Contenido web</div>
+        </template>
+        <template v-slot:menu-movil>Contenido movil</template>
+        <template v-slot:asideRight>
+          <div class="max-w-sm mr-auto">
+            Derecha
+            <router-view name="menu-right" />
+          </div>
+        </template>
+      </UserProfileLayout>
+      <router-view />
+    </div>
   </div>
 </template>
 <script>
+import UserProfileLayout from 'user-profile-layout';
+
+function data() {
+  return {
+    show: false,
+  };
+}
+
 export default {
   name: 'DefaultLayout',
+  components: {
+    UserProfileLayout,
+  },
+  data,
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+.activator {
+  @apply block;
+  @apply pl-4;
 
+  @media (min-width: 769px) {
+    @apply hidden;
+  }
+}
+
+.dl-layout-main-container.profile {
+  .dl-container-layout {
+    @apply grid grid-flow-col gap-4;
+
+    .dl-aside-container {
+      @apply h-full;
+
+      .wrapper-menu {
+        @apply border border-solid border-$secondaryGray;
+        @apply px-4;
+        @apply sticky;
+        @apply ml-auto;
+
+        max-width: 15rem;
+        top: 1rem;
+
+        .menu-section-title {
+          @apply text-$secondaryTextGray;
+          @apply uppercase;
+        }
+
+        .profile-avatar-section {
+          @apply pt-32;
+        }
+      }
+    }
+  }
+
+  .dl-menu-movil {
+    @apply bg-white;
+  }
+
+  .menu-items {
+    @apply w-full;
+    @apply pb-4 pt-6;
+    @apply rounded;
+    @apply text-$secondaryTextGray font-bold;
+    @apply sticky top-0;
+
+    li {
+      @apply my-2 px-3 py-1;
+      @apply text-left whitespace-no-wrap;
+      @apply duration-200;
+
+      &:hover {
+        @apply bg-$secondaryGray;
+      }
+
+      &.active {
+        @apply bg-$secondaryGray;
+        @apply w-full h-full;
+        @apply rounded-md;
+        @apply duration-500;
+      }
+    }
+  }
+
+  .wrapper-menu-movil {
+    @apply h-screen;
+    @apply bg-white;
+    @apply fixed z-10;
+    @apply border-r border-solid border-gray-200;
+
+    flex-basis: 22%;
+
+    .container-menu-movil {
+      .go-back {
+        @apply px-6 pt-4;
+      }
+    }
+  }
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,51 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+// const plugin = require('tailwindcss/plugin');
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
 module.exports = {
-  purge: [],
+  important: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        $black: '#000000',
+        error: defaultTheme.colors.red[600],
+        $primaryGray: '#ececec',
+        $primaryTextGray: '#222222',
+        $secondaryGray: '#f5f5f5',
+        $secondaryTextGray: '#666666',
+        $white: '#ffffff',
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      lineHeight: (theme) => theme('spacing'),
+      minWidth: {
+        movil: '32rem',
+      },
+      width: {
+        content: 'fit-content',
+      },
+    },
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    boxShadow: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    margin: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    fontSize: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+  },
   plugins: [],
-}
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+    ],
+  },
+};
